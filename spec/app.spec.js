fe929100a7e97b92c4135acd97a85f63a64fa2ff
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-08 09:35:22 +0100

    GET api/articles/article_id/comments reutns 400 if invalid article id

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -207,20 +207,20 @@ describe('/api/', () => {
             expect(body.comments[0].article_id).to.eql(1)
         })
     });
-    it.only('GET /api/articles/article_id/comments returns a 404 if there is a spelling error in the endpoint', () => {
+    it('GET /api/articles/article_id/comments returns a 404 if there is a spelling error in the endpoint', () => {
         return request(app)
         .get('/api/articles/1/commmments')
         .then(({body}) => {
             expect(body.msg).to.eql('Page Not Found')
         })
     });
-    // it('GET /api/articles/article_id/comments returns a 400 error if an invalid article_id is passed', () => {
-    //     return request(app)
-    //     .get('/api/articles/blackberries/comments')
-    //     .then(({body}) => {
-    //         expect(body.msg).to.eql('Bad Request')
-    //     })
-    // });
+    it.only('GET /api/articles/article_id/comments returns a 400 error if an invalid article_id is passed', () => {
+        return request(app)
+        .get('/api/articles/blackberries/comments')
+        .then(({body}) => {
+            expect(body.msg).to.eql('Bad Request')
+        })
+    });
     // it('GET /api/articles/article_id/comments returns a 404 error if the article does not exist', () => {
     //     return request(app)
     //     .get('/api/articles/164/comments')
