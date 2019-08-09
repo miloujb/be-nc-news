ab1e27a2e4c8b2f20e133ae96f325d03173955d1
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-09 13:04:26 +0100

    check for input length removed from utils

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,4 @@
 exports.formatDates = list => {
-    if (list.length == 0) return [];
     const formattedList = list.map(({created_at, ...restofItem}) => {
         return {created_at: new Date(created_at), ... restofItem};
     })
@@ -7,7 +6,6 @@ exports.formatDates = list => {
 };
 
 exports.makeRefObj = list => {
-    if (list.length === 0) return {}
     let obj = {}
     list.forEach(item => {
         obj[item.title]=item.article_id
@@ -16,7 +14,6 @@ exports.makeRefObj = list => {
 };
 
 exports.formatComments = (comments, articleRef) => {
-    if (comments.length === 0) return [];
     return comments.map(comment => {
     const {created_by, created_at, belongs_to, ...restOfComment} = comment;
     const obj = {...restOfComment};
