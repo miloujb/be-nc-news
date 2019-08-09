58ef1c4259add4d89ae651b87cc295000000c73b
miloujb <emilyjbailey@hotmail.co.uk>
2019-08-09 12:57:52 +0100

    redundant variable removed from getComments

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -39,7 +39,6 @@ const getArticles = (req, res, next) => {
 
 const getComments = (req, res, next) => {
     const {article_id} = req.params
-    const sort_by = req.query
     fetchComments(article_id, req.query)
     .then(comments => {
         res.status(200).send({comments})
